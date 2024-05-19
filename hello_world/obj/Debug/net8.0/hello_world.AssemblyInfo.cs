e4ccb2dc0faac7a33cdc79ed9526d874fe042317
tal.shaham <talso12345@gmail.com>
2024-05-19 11:12:36 +0300

    first commit

--- hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
+++ hello_world/obj/Debug/net8.0/hello_world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eeeaefc525f7cea10aaa700333dbe195a15e34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cb5bee22bcfd0c4572559e2c3c74697438b167f")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello_world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
